b863989336473e70d149e4cc181bde06fb662cd4
ferniwob <jordi@wob3.com>
2014-06-10 11:57:40 +0200

    Codeigniter 2.2 french languaje
    Codeigniter 2.2 french languaje

---
+++ french/autobrill_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$lang['autobrill_titulo'] = 'Plus de 40 ans dans la production assainisseurs avec vous - Autobrill';
+$lang['autobrill_titulo_seccion'] = 'LEBO S.L.U.';
+
+
---
+++ french/home_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$lang['home_titulo'] = "Désodorisants et produits de nettoyage et d'entretien pour votre voiture - Autobrill";
+$lang['home_titulo_seccion'] = 'LEBO S.L.U.';
+
+
