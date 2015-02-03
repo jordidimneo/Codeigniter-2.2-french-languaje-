c28f70a1caa968c7791728c8a163649e5fe00488
Jordi <jordi@wob3.com>
2015-02-03 11:49:03 +0100

    Update form_validation_lang.php

--- french/form_validation_lang.php
+++ french/form_validation_lang.php
@@ -6,9 +6,9 @@ $lang['valid_email']			= "Le champ %s doit contenir une adresse email valide.";
 $lang['valid_emails']			= "Le champ %s ne peut contenir que des adresses email valides.";
 $lang['valid_url']				= "Le champ %s doit contenir une URL valide.";
 $lang['valid_ip']				= "Le champ %s doit contenir une IP valide.";
-$lang['min_length']				= "Le champ %s doit contenir au moins {param} caract&egrave;res.";
-$lang['max_length']				= "Le champ %s ne peut contenir plus de {param} caract&egrave;res.";
-$lang['exact_length']			= "Le champ %s doit contenir exactement {param} caract&egrave;res.";
+$lang['min_length']				= "Le champ %s doit contenir au moins %s caract&egrave;res.";
+$lang['max_length']				= "Le champ %s ne peut contenir plus de %s caract&egrave;res.";
+$lang['exact_length']			= "Le champ %s doit contenir exactement %s caract&egrave;res.";
 $lang['alpha']					= "Le champ %s ne peut contenir que des caract&egrave;res alphab&eacute;tiques.";
 $lang['alpha_numeric']			= "Le champ %s ne peut contenir que des caract&egrave;res alphanum&eacute;riques.";
 $lang['alpha_dash']				= "Le champ %s ne peut contenir que des caract&egrave;res alphanum&eacute;riques, des tirets bas (underscore) et des traits d'union.";
@@ -16,14 +16,14 @@ $lang['numeric']				= "Le champ %s doit contenir un nombre (caract&egrave;res nu
 $lang['is_numeric']				= "Le champ %s ne peut contenir que des signes du type nombre.";
 $lang['integer']				= "Le champ %s doit contenir un integer.";
 $lang['regex_match']			= "Le champ %s n'utilise pas le bon format.";
-$lang['matches']				= "Le champ %s doit correspondre au champ {param}.";
-$lang['form_validation_differs']				= 'Le champ %s doit être différent du champ {param}.';
+$lang['matches']				= "Le champ %s doit correspondre au champ %s.";
+$lang['form_validation_differs']				= 'Le champ %s doit être différent du champ %s.';
 $lang['is_unique'] 				= "Le champ %s doit contenir une valeur unique.";
 $lang['is_natural']				= "Le champ %s ne peut contenir que des nombres positifs.";
 $lang['is_natural_no_zero']		= "Le champ %s ne peut contenir que des nombres plus grands que z&eacute;ro.";
 $lang['decimal']				= "Le champ %s doit contenir un nombre d&eacute;cimal.";
-$lang['less_than']				= "Le champ %s doit contenir un nombre inf&eacute;rieur &agrave; {param}.";
-$lang['greater_than']			= "Le champ %s doit contenir un nombre sup&eacute;rieur &agrave; {param}.";
+$lang['less_than']				= "Le champ %s doit contenir un nombre inf&eacute;rieur &agrave; %s.";
+$lang['greater_than']			= "Le champ %s doit contenir un nombre sup&eacute;rieur &agrave; %s.";
 
 
 
